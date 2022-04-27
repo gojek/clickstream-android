69a0799a3a5d27a4e28d284610c6ddccebbc2a46
radityagumay <gumay.raditya@gmail.com>
2022-04-27 19:25:07 +0700

    [Spike] : Add Sonatype Publisher

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,11 @@ apply(plugin = "binary-compatibility-validator")
 
 buildscript {
     repositories {
-        google()
-        maven { setUrl("https://maven-central-asia.storage-download.googleapis.com/repos/central/data") }
+        maven(url = "https://plugins.gradle.org/m2/")
+        maven(url = "https://maven-central-asia.storage-download.googleapis.com/repos/central/data")
         mavenCentral()
+        google()
+        jcenter()
     }
 
     dependencies {
@@ -16,6 +18,7 @@ buildscript {
         classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.12")
         classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.2.4")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.40.5")
+        classpath("io.github.gradle-nexus:publish-plugin:1.1.0")
     }
 }
 
@@ -29,3 +32,6 @@ val clean by tasks.creating(Delete::class) {
     delete("${rootDir}/clickstream/build")
     delete("${rootDir}/report")
 }
+
+apply(plugin = "io.github.gradle-nexus.publish-plugin")
+apply(from = "${rootDir}/scripts/publish-root.gradle")
